9d4ffb961fb5b2a4d8a186dd70f14fadaef0d731
bhavesh089 <bhavesh089git@gmail.com>
2020-06-04 16:28:15 +0530

    committed!

---
+++ main.py
@@ -0,0 +1,17 @@
+# Server
+import uvicorn
+from fastapi import FastAPI
+import json
+
+# Data Handling
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.post("/Body")
+async def someMethod(body: dict):
+    fileName = 'Response_data.json'
+    with open(fileName, "w+") as write_file:
+        json.dump(body,write_file)
+    return fileName
