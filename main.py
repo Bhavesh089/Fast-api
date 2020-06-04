d76ed13162bde6b8d49048f93bf7821cb935233d
bhavesh089 <bhavesh089git@gmail.com>
2020-06-04 18:48:13 +0530

    committed!

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-# Server
-import uvicorn
+  
 from fastapi import FastAPI
-import json
-
-# Data Handling
-from pydantic import BaseModel
 
 app = FastAPI()
 
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-@app.post("/body")
-def someMethod(body: dict):
-    fileName = 'Response_data.json'
-    with open(fileName, "w+") as write_file:
-        json.dump(body,write_file)
-    return fileName
 
 @app.get("/")
-def hello():
+def home():
     return {"message":"Hello TutLinks.com"}
+
+
+
+
+# # Server
+# import uvicorn
+# from fastapi import FastAPI
+# import json
+
+# # Data Handling
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+
+# @app.post("/body")
+# def someMethod(body: dict):
+#     fileName = 'Response_data.json'
+#     with open(fileName, "w+") as write_file:
+#         json.dump(body,write_file)
+#     return fileName
+
+# @app.get("/")
+# def hello():
+#     return {"message":"Hello TutLinks.com"}
