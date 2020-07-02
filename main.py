4a522c727aed5b8894476ed64ab7e041e14860e9
Bhavesh089 <65395242+Bhavesh089@users.noreply.github.com>
2020-07-02 19:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ app = FastAPI()
 
 @app.post("/body")
 def someMethod(body: dict):
-#     fileName = 'Response_data.json'
-#     with open(fileName, "w+") as write_file:
-#         json.dump(body,write_file)
+    fileName = 'Response_data.json'
+    with open(fileName, "w+") as write_file:
+        json.dump(body,write_file)
     return body
+
+@app.get("/readingfile")
+def ReadingJson():
+ with open('Response_data.json') as f:
+  data = json.load(f)
+    return data    
 
 @app.get("/")
 def hello():
