b6404f0fb49be158f87aff8ed5879812815487fc
bhaveshgit <bhavesh089git@gmail.com>
2020-08-07 10:37:38 +0530

    added redirect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  # Server
+# Server
 import uvicorn
 from fastapi import FastAPI
 import json
+from starlette.responses import RedirectResponse
 
 # Data Handling
 from pydantic import BaseModel
@@ -11,19 +12,22 @@ app = FastAPI()
 
 @app.post("/")
 def someMethod(body: dict):
-    
+
     fileName = 'Response_data.json'
     with open(fileName, "w+") as write_file:
-        json.dump(body,write_file)
+        json.dump(body, write_file)
     return body
+
 
 @app.get("/readingfile")
 def ReadingJson():
- with open('Response_data.json') as f:
-    data = json.load(f)
-    return data    
+    with open('Response_data.json') as f:
+        data = json.load(f)
+        return data
+
 
 @app.get("/body")
-def hello(code:str):
-    response = RedirectResponse(url="http://localhost:3000/auth/lazada/redirect?code="+code)
+def hello(code: str):
+    response = RedirectResponse(
+        url="http://localhost:3000/auth/lazada/redirect?code="+code)
     return response
